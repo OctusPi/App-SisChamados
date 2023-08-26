dc3f36cc19a8b56336f085a8e076baa7ed0af13a
OctusPi <octuspi@gmail.com>
2023-08-26 12:36:23 -0300

    add: JsonReques and Read&Write Files Utilitary Class

--- app/src/main/java/br/com/dticampossales/appsischamados/MainActivity.java
+++ app/src/main/java/br/com/dticampossales/appsischamados/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String a = String.valueOf(R.string.login);
+        getApplicationContext();
 
     }
 }
