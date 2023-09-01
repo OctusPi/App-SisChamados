220d03796a6315eb6edccca13b22756e4915f9ce
Renan <alvesrenanpro@gmail.com>
2023-09-01 04:26:12 -0300

    feat: filter function added

--- app/src/main/java/br/com/dticampossales/appsischamados/ChamadosActivity.java
+++ app/src/main/java/br/com/dticampossales/appsischamados/ChamadosActivity.java
@@ -6,12 +6,14 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import org.json.JSONObject;
 
@@ -20,33 +22,50 @@ import java.util.ArrayList;
 import Utils.JsonUtil;
 import Utils.RawJsonReader;
 import br.com.dticampossales.appsischamados.adapters.Chamados.ChamadosListAdapter;
-import br.com.dticampossales.appsischamados.adapters.Chamados.ChamadosSpinnerAdapter;
 
 public class ChamadosActivity extends AppCompatActivity {
+    private ArrayList<JSONObject> dataSource;
+    private ChamadosListAdapter chamadosListAdapter;
+
+    private ArrayList<JSONObject> sectorFilter;
+    private ArrayList<JSONObject> equipmentFilter;
+    private ArrayList<JSONObject> statusFilter;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chamados);
 
-        populateRecyclerView(findViewById(R.id.chamados_list),
-                RawJsonReader.makeDataSource(this, R.raw.data_test));
+        dataSource = RawJsonReader.makeDataSource(this, R.raw.data_test);
+        chamadosListAdapter = new ChamadosListAdapter(dataSource);
+
+        sectorFilter = RawJsonReader.makeDataSource(this, R.raw.filter_sector_example);
+        equipmentFilter = RawJsonReader.makeDataSource(this, R.raw.filter_equipment_example);
+        statusFilter = RawJsonReader.makeDataSource(this, R.raw.filter_status_example);
+
+        populateRecyclerView(findViewById(R.id.chamados_list), chamadosListAdapter);
 
         FloatingActionButton floatingActionButton = findViewById(R.id.chamados_floating);
         floatingActionButton.setOnClickListener(view -> toggleFilterLayout());
 
-        makeSpinnerItems(findViewById(R.id.filter_sector),
-                RawJsonReader.makeDataSource(this, R.raw.filter_sector_example));
+        Spinner sectorSpinner = findViewById(R.id.filter_sector);
+        Spinner equipmentSpinner = findViewById(R.id.filter_equipment);
+        Spinner statusSpinner = findViewById(R.id.filter_status);
 
-        makeSpinnerItems(findViewById(R.id.filter_equipment),
-                RawJsonReader.makeDataSource(this, R.raw.filter_equipment_example));
+        makeSpinnerItems(sectorSpinner, sectorFilter);
+        makeSpinnerItems(equipmentSpinner, equipmentFilter);
+        makeSpinnerItems(statusSpinner, statusFilter);
 
-        makeSpinnerItems(findViewById(R.id.filter_status),
-                RawJsonReader.makeDataSource(this, R.raw.filter_status_example));
+        Button filterBtn = findViewById(R.id.filter_button);
+        filterBtn.setOnClickListener(view -> {
+            Log.println(Log.ASSERT, "msg", sectorSpinner.getSelectedItem().toString());
+            filter("sector", sectorSpinner.getSelectedItem().toString(), dataSource);
+        });
     }
 
-    private void populateRecyclerView(RecyclerView recyclerView, ArrayList<JSONObject> dataSource) {
+    private void populateRecyclerView(RecyclerView recyclerView, ChamadosListAdapter adapter) {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(new ChamadosListAdapter(dataSource));
+        recyclerView.setAdapter(adapter);
     }
 
     private void toggleFilterLayout() {
@@ -58,8 +77,21 @@ public class ChamadosActivity extends AppCompatActivity {
         }
     }
 
+    private void filter(String key, String value, ArrayList<JSONObject> dataSet) {
+        ArrayList<JSONObject> filteredList = new ArrayList<>();
+        if (!value.equals(getString(R.string.filter_default))) {
+            for (int i = 0; i < dataSet.size(); i++) {
+                if (JsonUtil.getJsonVal(dataSet.get(i), key).equals(value)) {
+                    filteredList.add(dataSet.get(i));
+                    Log.println(Log.ASSERT, "msg", JsonUtil.getJsonVal(dataSet.get(i), key));
+                }
+            }
+            chamadosListAdapter.applyFilter(filteredList);
+        }
+    }
+
     private void makeSpinnerItems(Spinner spinner, ArrayList<JSONObject> optionsList) {
-        ArrayList<CharSequence> optionsString = new ArrayList<CharSequence>();
+        ArrayList<CharSequence> optionsString = new ArrayList<>();
 
         optionsString.add(getString(R.string.filter_default));
 
@@ -72,7 +104,5 @@ public class ChamadosActivity extends AppCompatActivity {
 
         arrayAdapter.setDropDownViewResource(R.layout.spinner_dropdown);
         spinner.setAdapter(arrayAdapter);
-
-        spinner.setOnItemSelectedListener(new ChamadosSpinnerAdapter());
     }
 }
